e5b1f1a284cc0438553b141d1fc6d6570ae96e7e
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-02 00:16:49 -0700

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace RegexPracticeProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the EmailId");
+            string code = Console.ReadLine();
+
+            Pattern patterns = new Pattern();
+            bool val = patterns.validatePincode(code);
+
+            if (val)
+            {
+                Console.WriteLine("Matched");
+            }
+            else
+            {
+                Console.WriteLine("Not Matched");
+            }
+        }
+    }
+}
